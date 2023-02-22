e0da80cc9762c760612bf5620ec704e1953507f9
Arnoldas R <arokon.arnoldas.reklys@gmail.com>
2023-02-22 17:41:37 +0200

    Added  react frontend

---
+++ uploader/uploaderfrontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormCSV from './components/Form';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <FormCSV/>
+    </div>
+  );
+}
+
+export default App;
